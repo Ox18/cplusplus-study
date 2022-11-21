6d5070299b72f70ab539f4e0cb498e139044a255
Alex <wdelgadoalama@gmail.com>
2022-11-20 21:24:24 -0500

    add p3

---
+++ p3/app.cpp
@@ -0,0 +1,16 @@
+// i/o data
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int number;
+	
+	cout<<"please, digit a number: ";
+	cin>>number;
+	
+	cout<<"\nThe number digited is: "<<number;
+	
+	return 0;
+}
