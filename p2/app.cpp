721250560d8e4ce0bce8285b278c88cf9323453d
Alex <wdelgadoalama@gmail.com>
2022-11-20 21:23:06 -0500

    remove app

--- p2/app.cpp
+++ p2/app.cpp
@@ -7,9 +7,13 @@ using namespace std;
 int main(){
 	int integer = 15;
 	float pi = 3.1416;
+	double mayor = 16.45846;
+	char letra = 'W';
 	
 	cout<<integer<<endl;
-	cout<<pi;
+	cout<<pi<<endl;
+	cout<<mayor<<endl;
+	cout<<letra<<endl;
 	
 	return 0;
 }
